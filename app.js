7f808b4dcaaa15cbda7e63c9e9e35306948ba832
saumikkhan <saumikkhan@crescentschool.org>
2021-01-16 01:54:02 -0500

    add cert

--- app.js
+++ app.js
@@ -20,21 +20,71 @@ app.get("/", function (req,res) {
 
 let URI = "postgres://abdulrahman:d1dkUYmnI1dqkSmY@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/pastel-vole-236.defaultdb?sslmode=verify-full&sslrootcert=cc-ca.crt/cc-ca.crt"
 let cert_dir = __dirname + "/ssl/cc-ca.crt";
+console.log(cert_dir);
 
-const sequelize = new Sequelize('postgres://abdulrahman:d1dkUYmnI1dqkSmY@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/pastel-vole-236.defaultdb?sslmode=verify-full&sslrootcert=' + cert_dir);
-
+// const sequelize = new Sequelize('postgres://abdulrahman:d1dkUYmnI1dqkSmY@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/pastel-vole-236.defaultdb?sslmode=verify-full&sslrootcert=' + cert_dir);
+//
+// const User = sequelize.define('User', {
+//     // Model attributes are defined here
+//     firstName: {
+//         type: String,
+//         allowNull: false
+//     },
+//     lastName: {
+//         type: String
+//         // allowNull defaults to true
+//     }
+// }, {
+//     // Other model options go here
+// });
+// sequelize.query('show tables').then(function(rows) {
+//     console.log(JSON.stringify(rows));
+// });
+//
+// // `sequelize.define` also returns the model
+// console.log(User === sequelize.models.User); // true
+// try {
+//     sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+// } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+// }
 app.get("/start", function (req,res){
     res.render("start");
 })
 
+//trying it oout the oop way
+// Connect to CockroachDB through Sequelize.
+let sequelize = new Sequelize('defaultdb/', 'abdulrahman', 'Iloveabdul123', {
+    host: 'free-tier.gcp-us-central1.cockroachlabs.cloud',
+    dialect: 'postgres',
+    port: 26257,
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            ca: fs.readFileSync('ssl/cc-ca.crt')
+                .toString()
+        }
+    }
+});
 
 
 
 
+// let dialectOptions = {
+//     ssl: {
+//         ca: fs.readFileSync("ssl/cc-ca.crt")
+//             .toString()
+//     }
+// }
 
 
-
-
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 
 
